a81b9d0272445afda942453516e2ddb7d8b6e91a
Partho Debnath <parthodebnath28@gmail.com>
2024-09-24 23:24:51 +0600

    update specific document, update by filter, delete specific document, delete by filter, export data and search documents functionality added

--- main.py
+++ main.py
@@ -67,5 +67,61 @@ def delete_collection():
 
 
 def get_specific_document():
-    data = client.collections["books"].documents["10001"].retrieve()
+    document_id = "10001"
+    data = client.collections["books"].documents[document_id].retrieve()
     print(data)
+
+
+def update_specific_document():
+    document_id = "10001"
+    data = {
+        "authors": ["John Keegan-2"],
+        "average_rating": 5,
+        "title": "The First World War-2",
+    }
+    client.collections["books"].documents[document_id].update(data)
+
+
+def update_by_filter():
+    data = {
+        "title": "The First World War-2 (Best)",
+    }
+    response = client.collections["books"].documents.update(
+        data, {"filter_by": "average_rating:>4"}
+    )
+    print(response)
+
+
+def delete_specific_document():
+    document_id = "10001"
+    response = client.collections["books"].documents[document_id].delete()
+    print(response)
+
+
+def delete_by_filter():
+    response = client.collections["books"].documents.delete(
+        {"filter_by": "average_rating:>5"}
+    )
+    print(response)
+
+
+def export_data():
+    data = client.collections["books"].documents.export()
+    print(data)
+
+
+def search_documents():
+    search_parameters = {
+        "q": "*",
+        "query_by": "",
+        "filter_by": "title:The First World War-2",
+        "sort_by": "average_rating:desc",
+        "per_page": 10,
+    }
+
+    response = client.collections["books"].documents.search(search_parameters)
+    print(response)
+    print(len(response.get("hits")))
+
+
+search_documents()
